c36b53822f84df6b8d7b910068c92255f4c016ef
marinabre <marina.brebric@fer.hr>
2015-05-10 23:31:47 +0200

    added controllers for EF models

---
+++ GuessWhere/Controllers/GamesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GuessWhere.Models;
+
+namespace GuessWhere.Controllers
+{
+    public class GamesController : Controller
+    {
+        private Guess_WhereEntities1 db = new Guess_WhereEntities1();
+
+        // GET: Games
+        public ActionResult Index()
+        {
+            var game = db.Game.Include(g => g.Image).Include(g => g.Image1).Include(g => g.Image2).Include(g => g.Image3).Include(g => g.Image4).Include(g => g.Image5).Include(g => g.Image6);
+            return View(game.ToList());
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDimg1 = new SelectList(db.Image, "IDimage", "image");
+            ViewBag.IDimg2 = new SelectList(db.Image, "IDimage", "image");
+            ViewBag.IDimg3 = new SelectList(db.Image, "IDimage", "image");
+            ViewBag.IDimg4 = new SelectList(db.Image, "IDimage", "image");
+            ViewBag.IDimg5 = new SelectList(db.Image, "IDimage", "image");
+            ViewBag.IDimg6 = new SelectList(db.Image, "IDimage", "image");
+            ViewBag.IDimg7 = new SelectList(db.Image, "IDimage", "image");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDgame,IDimg1,IDimg2,IDimg3,IDimg4,IDimg5,IDimg6,IDimg7")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Game.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDimg1 = new SelectList(db.Image, "IDimage", "image", game.IDimg1);
+            ViewBag.IDimg2 = new SelectList(db.Image, "IDimage", "image", game.IDimg2);
+            ViewBag.IDimg3 = new SelectList(db.Image, "IDimage", "image", game.IDimg3);
+            ViewBag.IDimg4 = new SelectList(db.Image, "IDimage", "image", game.IDimg4);
+            ViewBag.IDimg5 = new SelectList(db.Image, "IDimage", "image", game.IDimg5);
+            ViewBag.IDimg6 = new SelectList(db.Image, "IDimage", "image", game.IDimg6);
+            ViewBag.IDimg7 = new SelectList(db.Image, "IDimage", "image", game.IDimg7);
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDimg1 = new SelectList(db.Image, "IDimage", "image", game.IDimg1);
+            ViewBag.IDimg2 = new SelectList(db.Image, "IDimage", "image", game.IDimg2);
+            ViewBag.IDimg3 = new SelectList(db.Image, "IDimage", "image", game.IDimg3);
+            ViewBag.IDimg4 = new SelectList(db.Image, "IDimage", "image", game.IDimg4);
+            ViewBag.IDimg5 = new SelectList(db.Image, "IDimage", "image", game.IDimg5);
+            ViewBag.IDimg6 = new SelectList(db.Image, "IDimage", "image", game.IDimg6);
+            ViewBag.IDimg7 = new SelectList(db.Image, "IDimage", "image", game.IDimg7);
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDgame,IDimg1,IDimg2,IDimg3,IDimg4,IDimg5,IDimg6,IDimg7")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDimg1 = new SelectList(db.Image, "IDimage", "image", game.IDimg1);
+            ViewBag.IDimg2 = new SelectList(db.Image, "IDimage", "image", game.IDimg2);
+            ViewBag.IDimg3 = new SelectList(db.Image, "IDimage", "image", game.IDimg3);
+            ViewBag.IDimg4 = new SelectList(db.Image, "IDimage", "image", game.IDimg4);
+            ViewBag.IDimg5 = new SelectList(db.Image, "IDimage", "image", game.IDimg5);
+            ViewBag.IDimg6 = new SelectList(db.Image, "IDimage", "image", game.IDimg6);
+            ViewBag.IDimg7 = new SelectList(db.Image, "IDimage", "image", game.IDimg7);
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Game game = db.Game.Find(id);
+            db.Game.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GuessWhere/Controllers/LeaderBoardsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GuessWhere.Models;
+
+namespace GuessWhere.Controllers
+{
+    public class LeaderBoardsController : Controller
+    {
+        private Guess_WhereEntities1 db = new Guess_WhereEntities1();
+
+        // GET: LeaderBoards
+        public ActionResult Index()
+        {
+            var leaderBoard = db.LeaderBoard.Include(l => l.Game).Include(l => l.User);
+            return View(leaderBoard.ToList());
+        }
+
+        // GET: LeaderBoards/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LeaderBoard leaderBoard = db.LeaderBoard.Find(id);
+            if (leaderBoard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(leaderBoard);
+        }
+
+        // GET: LeaderBoards/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDgame = new SelectList(db.Game, "IDgame", "IDgame");
+            ViewBag.IDuser = new SelectList(db.User, "IDuser", "username");
+            return View();
+        }
+
+        // POST: LeaderBoards/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDboard,IDuser,IDgame,username,score")] LeaderBoard leaderBoard)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LeaderBoard.Add(leaderBoard);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDgame = new SelectList(db.Game, "IDgame", "IDgame", leaderBoard.IDgame);
+            ViewBag.IDuser = new SelectList(db.User, "IDuser", "username", leaderBoard.IDuser);
+            return View(leaderBoard);
+        }
+
+        // GET: LeaderBoards/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LeaderBoard leaderBoard = db.LeaderBoard.Find(id);
+            if (leaderBoard == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDgame = new SelectList(db.Game, "IDgame", "IDgame", leaderBoard.IDgame);
+            ViewBag.IDuser = new SelectList(db.User, "IDuser", "username", leaderBoard.IDuser);
+            return View(leaderBoard);
+        }
+
+        // POST: LeaderBoards/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDboard,IDuser,IDgame,username,score")] LeaderBoard leaderBoard)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(leaderBoard).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDgame = new SelectList(db.Game, "IDgame", "IDgame", leaderBoard.IDgame);
+            ViewBag.IDuser = new SelectList(db.User, "IDuser", "username", leaderBoard.IDuser);
+            return View(leaderBoard);
+        }
+
+        // GET: LeaderBoards/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LeaderBoard leaderBoard = db.LeaderBoard.Find(id);
+            if (leaderBoard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(leaderBoard);
+        }
+
+        // POST: LeaderBoards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LeaderBoard leaderBoard = db.LeaderBoard.Find(id);
+            db.LeaderBoard.Remove(leaderBoard);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
