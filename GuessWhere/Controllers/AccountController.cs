2aaaebd6f320c259c9c6013bd9bf752989897b2c
lfadljevic <leon.fadljevic@fer.hr>
2015-05-15 12:52:34 +0200

    adding username on registration form

--- GuessWhere/Controllers/AccountController.cs
+++ GuessWhere/Controllers/AccountController.cs
@@ -76,7 +76,7 @@ namespace GuessWhere.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -152,17 +152,18 @@ namespace GuessWhere.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Username, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
 
                 context = new Guess_WhereEntities1();
 
                 // prije svega dodaj tog korisnika u tablicu User 
-                // (kako ne bi neki anonimni mogao "ukrasti identitet" i potpisati svoj score kao nečiji tuđi identitet
+                // (kako ne bi neki anonimni mogao "ukrasti identitet" i potpisati svoj score kao nečiji tuđi identitet)
                 context.User.Add(new User
                 {
-                    username = model.Email
+                    username = model.Username
                 });
+                
                 // zatim dodaj korisnika koji se želi registrirati u tablicu registriranih korisnika (RegisteredUser)
                 context.RegisteredUser.Add(new RegisteredUser
                 {
@@ -173,9 +174,9 @@ namespace GuessWhere.Controllers
                 context.SaveChanges();
 
                 if (result.Succeeded)
-                {    
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     //string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
