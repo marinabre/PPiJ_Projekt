38cefa0f0cdd4df9cf046d6644a15d25c25ebf4f
marinabre <marina.brebric@fer.hr>
2015-05-17 20:38:08 +0200

    small changes

--- GuessWhere/Models/Image.cs
+++ GuessWhere/Models/Image.cs
@@ -11,6 +11,7 @@ namespace GuessWhere.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Image
     {
@@ -31,7 +32,12 @@ namespace GuessWhere.Models
         public string hint1 { get; set; }
         public string hint2 { get; set; }
         public string info { get; set; }
+
+        [UIHint("Decimal")]
+        [DisplayFormat(DataFormatString = "{0:0.0000}", ApplyFormatInEditMode = true)]
         public decimal latitude { get; set; }
+        [UIHint("Decimal")]
+        [DisplayFormat(DataFormatString = "{0:0.0000}", ApplyFormatInEditMode = true)]
         public decimal longitude { get; set; }
     
         public virtual ICollection<Game> Game { get; set; }
