21616ec1ab424cf19329c8941fc5eace3ac5a7f9
lfadljevic <leon.fadljevic@fer.hr>
2015-05-19 03:20:37 +0200

    adding AvatarController and functionality over it

---
+++ GuessWhere/Controllers/AvatarController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using GuessWhere.Models;
+
+namespace GuessWhere.Controllers
+{
+    [Authorize]
+    public class AvatarController : Controller
+    {
+        Guess_WhereEntities1 context;
+
+        // GET: Avatar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
